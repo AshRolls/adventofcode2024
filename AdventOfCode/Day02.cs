c8cb308c5aa4362cb275eba8d1f86ffef101dcb2
Ashley Rolleston <ashforcash@gmail.com>
2024-12-05 10:09:54 +0000

    solve day 2 part 2

--- AdventOfCode/Day02.cs
+++ AdventOfCode/Day02.cs
@@ -1,4 +1,8 @@
-﻿namespace AdventOfCode;
+﻿
+
+using System.ComponentModel.Design;
+
+namespace AdventOfCode;
 
 public class Day02 : BaseDay
 {
@@ -55,6 +59,56 @@ public class Day02 : BaseDay
 
     private void solve2()
     {
-        _partTwo = "Not Solved";
+        int safeCount = 0;
+        foreach (var input in _input)
+        {
+            var nums = AoCHelper.GetNumsFromStr(input);
+            if (checkSequence(nums)) { safeCount++; continue; }
+
+            for (int x = 0; x < nums.Length; x++)
+            {
+                var numsRemoved = nums.ToList();
+                numsRemoved.RemoveAt(x);
+                if (checkSequence(numsRemoved.ToArray())) { safeCount++; break; }
+            }
+        }
+
+        _partTwo = safeCount.ToString();
+    }
+
+    private bool getNegative(int[] nums)
+    {
+        return (nums[1] - nums[0] < 0);
+    }
+
+    private bool checkSequence(int[] nums)
+    {
+        bool negative = getNegative(nums);
+        for (int x = 0; x < nums.Length - 1; x++)
+        {
+            if (!checkValid(nums[x], nums[x + 1], negative)) return false;
+        }
+        return true;
+    }
+
+    private bool checkValid(int v1, int v2, bool negative)
+    {
+        int diff = v1 - v2;
+        if (diff == 0)
+        {
+            return false;
+        }
+        if (negative)
+        {
+            if (diff > 3 || diff <= 0)
+            {
+                return false;
+            }
+        }
+        else if (diff < -3 || diff >= 0)
+        {
+            return false;
+        }
+        return true;
     }
 }
