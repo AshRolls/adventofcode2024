8138ee7ff77a9ed056c90fa90b56a2eca09a33d9
Ashley Rolleston <ashforcash@gmail.com>
2024-12-08 13:28:08 +0000

    solve day 4 part 2

--- AdventOfCode/Day04.cs
+++ AdventOfCode/Day04.cs
@@ -22,13 +22,33 @@ public class Day04 : BaseDay
     {
         int h = _input.Length;
         int w = _input[0].Length;
-        char[,] grid = new char[h+6,w+6];
+        char[,] grid = new char[h + 6, w + 6];
 
+        parseGrid(h, w, grid);
 
+        int matches = 0;
+        for (int y = 0; y < h + 6; y++)
+        {
+            //Console.Out.WriteLine();
+            for (int x = 0; x < w + 6; x++)
+            {
+                //Console.Out.Write(grid[x, y]);
+                if (grid[x, y] == 'X')
+                {
+                    matches += checkX(grid, x, y);
+                }
+            }
+        }
+
+        _partOne = matches.ToString();
+    }
+
+    private void parseGrid(int h, int w, char[,] grid)
+    {
         for (int y = 0; y < h + 6; y++)
         {
             for (int x = 0; x < w + 6; x++)
-            { 
+            {
                 grid[x, y] = '.';
             }
         }
@@ -40,23 +60,6 @@ public class Day04 : BaseDay
                 grid[x, y] = _input[y - 3][x - 3];
             }
         }
-
-
-        int matches = 0;
-        for (int y=0;y<h+6;y++)
-        {
-            Console.Out.WriteLine();
-            for (int x=0;x<w+6;x++)
-            {
-                Console.Out.Write(grid[x, y]);
-                if (grid[x, y] == 'X')
-                {
-                    matches += checkX(grid, x, y);
-                }
-            }
-        }
-
-        _partOne = matches.ToString();
     }
 
     private int checkX(char[,] grid, int x, int y)
@@ -79,6 +82,37 @@ public class Day04 : BaseDay
 
     private void solve2()
     {
-        _partTwo = "Not Solved";
+        int h = _input.Length;
+        int w = _input[0].Length;
+        char[,] grid = new char[h + 6, w + 6];
+
+        parseGrid(h, w, grid);
+
+        int matches = 0;
+        for (int y = 0; y < h + 6; y++)
+        {
+            //Console.Out.WriteLine();
+            for (int x = 0; x < w + 6; x++)
+            {
+                //Console.Out.Write(grid[x, y]);
+                if (grid[x, y] == 'A')
+                {
+                    if (checkA(grid, x, y)) matches++;
+                }
+            }
+        }
+
+        _partTwo = matches.ToString();
+    }
+
+    private bool checkA(char[,] grid, int x, int y)
+    { 
+        bool first = false;
+        if ((grid[x + 1, y + 1] == 'M' && grid[x - 1, y - 1] == 'S') || (grid[x + 1, y + 1] == 'S' && grid[x - 1, y - 1] == 'M')) first = true;
+
+        bool second = false;
+        if ((grid[x - 1, y + 1] == 'M' && grid[x + 1, y - 1] == 'S') || (grid[x - 1, y + 1] == 'S' && grid[x + 1, y - 1] == 'M')) second = true;
+
+        return first && second;
     }
 }
